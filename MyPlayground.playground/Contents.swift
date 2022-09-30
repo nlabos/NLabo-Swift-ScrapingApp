033e17b273f140f204d2d939475f7ccbb3a813ac
Yuki Takatsu <yukitakatsu6468@gmail.com>
2022-09-30 13:30:37 +0900

    完成プログラム

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -11,3 +11,52 @@ print(favoriteDictionary["Study"])
 if let studyValue = favoriteDictionary["Study"] {
     print(studyValue)
 }
+
+
+//20回で使うプレイグラウンド
+
+for i in 1..<10 {
+    print("i: \(i)")
+}
+
+for i in 1...10 {
+    print("i: \(i)")
+}
+
+let arrayIntData = [1, 2, 3, 4]
+
+for data in arrayIntData {
+    print("data: \(data)")
+}
+
+let arrayStringData = ["犬", "猫", "きつね", "たぬき"]
+
+for data in arrayStringData{
+    print("data: \(data)")
+}
+
+
+for i in 0..<arrayIntData.count {
+    print("\(arrayIntData[i])番目のデータは\(arrayStringData[i])です")
+    
+}
+
+var count = 0
+var total = 0
+
+while count < 10 {
+    count += 1
+    total += count
+}
+print("count: \(count) total: \(total)")
+
+var repeatCount = 0
+var repeatTotal = 0
+
+repeat {
+    repeatCount += 1
+    repeatTotal += repeatCount
+} while(repeatCount < 10)
+    
+print("repeatCount: \(repeatCount) repeatTotal\(repeatTotal)")
+
